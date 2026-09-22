506e9f61fd7fddba3d9c97faf1e53d365a089508
hroscoe287-design <hroscoe287@gmail.com>
2026-09-22 02:54:46 -0500

    Fix adapter debug logging syntax

--- pocket_adapter.py
+++ pocket_adapter.py
@@ -79,7 +79,7 @@ class PocketOptionAdapter:
         self._subscriptions: set[tuple[str, int]] = set()
         self._loop: asyncio.AbstractEventLoop | None = None
         self._client = None
-        self._bars: dict[tuple[str, int], dict] = {}\n        self._data_logged = False
+        self._bars: dict[tuple[str, int], dict] = {}
 
     @property
     def configured(self) -> bool:
